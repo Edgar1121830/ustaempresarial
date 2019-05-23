06d5af8f86f5b2acaf7e202399ab6016791eb5b3
SantiagoJimenezOrtega <49984788+SantiagoJimenezOrtega@users.noreply.github.com>
2019-05-23 08:45:53 -0500

    actualizacion ddl de finanzas
    se añade el codigo necesario para crear el log de cuentas

--- DLL_DATABASE_SCRIPTS/DDL_FINANZAS/DDL_FINANZAS.sql
+++ DLL_DATABASE_SCRIPTS/DDL_FINANZAS/DDL_FINANZAS.sql
@@ -6,6 +6,26 @@
 /*==============================================================*/
 /* Table: PLAN_CONTABLE                                         */
 /*==============================================================*/
+CREATE TABLE "FINANZAS".log_cuentas(
+	CODIGO_cuenta_nueva INTEGER PRIMARY KEY NOT NULL,
+	NOMBRE_cuenta_nueva CHARACTER VARYING (100) NOT NULL,
+	DESCRIPCION_cuenta_nueva  CHARACTER VARYING (300) NOT NULL,
+	TIPO_cuenta_nueva  INTEGER NOT NULL,
+	VIGENCIA_cuenta_nueva DATE NOT NULL
+);
+
+COMMENT ON TABLE "FINANZAS".log_cuentas
+    IS 'Tabla donde se almacenan cada cuenta que entra como nueva ';
+
+COMMENT ON COLUMN "FINANZAS".PLAN_CONTABLE.CODIGO IS 'Código de la cuenta nueva – PUC';
+COMMENT ON COLUMN "FINANZAS".PLAN_CONTABLE.NOMBRE IS 'NOMBRE de la cuenta nueva';
+COMMENT ON COLUMN "FINANZAS".PLAN_CONTABLE.DESCRIPCION IS 'Descripción de la cuenta nueva';
+COMMENT ON COLUMN "FINANZAS".PLAN_CONTABLE.TIPO IS 'Clase(1), grupo(2),cuenta(3), subcuenta(4)';
+COMMENT ON COLUMN "FINANZAS".PLAN_CONTABLE.VIGENCIA IS 'Fecha de inicio de VIGENCIA de la cuenta nueva';
+
+/*==============================================================*/
+/* Table: PLAN_CONTABLE                                         */
+/*==============================================================*/
 CREATE TABLE "FINANZAS".PLAN_CONTABLE(
 	CODIGO INTEGER PRIMARY KEY NOT NULL,
 	NOMBRE CHARACTER VARYING (100) NOT NULL,
